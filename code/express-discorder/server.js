e25fef9a18c8c4165184f0a0b7d776c9e42ca636
aarestigue <108528013+aarestigue@users.noreply.github.com>
2022-12-20 12:27:16 +0000

    Stripe Webhook for invoice paid finished

--- code/express-discorder/server.js
+++ code/express-discorder/server.js
@@ -9,30 +9,30 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send(`
   <html>
-    <head><title>Success!</title></head>
+    <head><title>Success today!</title></head>
     <body>
-      <h1>You did it!</h1>
+      <h1>You did it hubspot post!</h1>
       <img src="https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif" alt="Cool kid doing thumbs up" />
     </body>
   </html>
 `));
 
-app.post("/github", (req, res) => {
-  const content = ":wave: Hi mom!";
-  const avatarUrl = "https://media.giphy.com/media/3o7TKSjRrfIPjeiVyM/giphy.gif";
-  axios
-    .post(process.env.DISCORD_WEBHOOK_URL, {
-      content: content,
-      embeds: [
-        {
-          image: {
-            url: avatarUrl,
-          },
-        },
-      ],
-    })
-    .then((discordResponse) => {
-      console.log("Success!");
+app.post("/invoice-paid", (req, res) => {
+  const dealId = req.body.data.object.customer_name;
+  console.log('hubspot here!', dealId)
+  const data = {
+    "properties": {
+      "invoice_paid": "True"
+    } 
+  };
+  axios.patch(`${process.env.HUB_URL}/crm/v3/objects/deals/${dealId}`, data, {headers:{
+      "Authorization": `Bearer ${process.env.HUBSPOT_TOKEN}`,
+      "Content-Type": "application/json"
+      }}
+       )
+    .then((hubspotResponse) => {
+
+      console.log(`Success Hubspot! ${hubspotResponse}`);
       res.status(204).send();
     })
     .catch((err) => console.error(`Error sending to Discord: ${err}`));
